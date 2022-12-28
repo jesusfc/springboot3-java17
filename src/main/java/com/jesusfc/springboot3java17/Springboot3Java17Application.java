03cba156663e8772666009dfc43800a2ec8c2506
Jesus <jesus.fdez.caraballo@gmail.com>
2022-12-29 00:01:50 +0100

    Dockerization project and add MySql container

---
+++ src/main/java/com/jesusfc/springboot3java17/Springboot3Java17Application.java
@@ -0,0 +1,13 @@
+package com.jesusfc.springboot3java17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3Java17Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot3Java17Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/jesusfc/springboot3java17/Springboot3Java17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesusfc.springboot3java17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3Java17ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

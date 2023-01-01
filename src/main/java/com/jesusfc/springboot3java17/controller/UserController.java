c099da4f493803e93fc04352812996567eaba712
Jesus <jesus.fdez.caraballo@gmail.com>
2023-01-01 22:22:54 +0100

    Add config Docker

---
+++ src/main/java/com/jesusfc/springboot3java17/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.jesusfc.springboot3java17.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+    @GetMapping("/user")
+    public Integer getUserById(@RequestParam(value = "userId") int userId) {
+        return userId;
+    }
+}

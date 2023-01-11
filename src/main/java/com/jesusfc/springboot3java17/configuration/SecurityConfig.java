4476298d711060e3448694501a243204349a0070
Jesus <jesus.fdez.caraballo@gmail.com>
2023-01-11 15:52:55 +0100

    Add Spring Security

---
+++ src/main/java/com/jesusfc/springboot3java17/configuration/SecurityConfig.java
@@ -0,0 +1,7 @@
+package com.jesusfc.springboot3java17.configuration;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class SecurityConfig {
+}

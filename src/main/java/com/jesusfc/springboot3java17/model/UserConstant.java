38c03b3878883485dc5bf217a6a8e1f223f1d41e
Jesus <jesus.fdez.caraballo@gmail.com>
2023-02-01 12:13:27 +0100

    Update yaml configuration

---
+++ src/main/java/com/jesusfc/springboot3java17/model/UserConstant.java
@@ -0,0 +1,46 @@
+package com.jesusfc.springboot3java17.model;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public final class UserConstant {
+
+    public static final int NAME_MIN_CHAR = 2;
+    public static final int NAME_MAX_CHAR = 40;
+    public static final String NAME_PATTERN = "^[\\p{L}\\s\\-]+$";
+
+    public static final int USERNAME_MIN_CHAR = 3;
+    public static final int USERNAME_MAX_CHAR = 40;
+    public static final String USERNAME_PATTERN = "^[\\w\\d]+$";
+
+    public static final int EMAIL_MAX_CHAR = 50;
+    public static final String EMAIL_PATTERN = "^(?!.*[=*\\s])(?=.*[.])[\\w\\d\\W]+$";
+    public static final String USERNAME_EMAIL_PATTERN = "^(?!.*[=*\\s])[\\w\\d\\W]+$";
+
+    public static final int PASSWORD_MIN_CHAR = 8;
+    public static final int PASSWORD_MAX_CHAR = 125;
+    // Pattern must consist of at least  8 characters, one lower- and one uppercase alphabetical letter, one numeric number and one special character !@#$%^&_
+    public static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[\\W]).{8,125}$";
+
+    // Model messages (can be found in messages.properties files - User constant error messages)
+    public static final String FIRST_NAME_NOT_EMPTY = "err.user.not.empty.first.name";
+    public static final String LAST_NAME_NOT_EMPTY = "err.user.not.empty.last.name";
+    public static final String USERNAME_NOT_EMPTY = "err.user.not.empty.username";
+    public static final String EMAIL_NOT_EMPTY = "err.user.not.empty.email";
+    public static final String USERNAME_EMAIL_NOT_EMPTY = "err.user.not.empty.username.email";
+    public static final String PASSWORD_NOT_EMPTY = "err.user.not.empty.password";
+
+    public static final String FIRST_NAME_CHAR_LENGTH = "err.user.char.first.name";
+    public static final String LAST_NAME_CHAR_LENGTH = "err.user.char.last.name";
+    public static final String USERNAME_CHAR_LENGTH = "err.user.char.username";
+    public static final String EMAIL_CHAR_LENGTH = "err.user.char.email";
+    public static final String USERNAME_EMAIL_CHAR_LENGTH = "err.user.char.username.email";
+    public static final String PASSWORD_CHAR_LENGTH = "err.user.char.password";
+
+    public static final String FIRST_NAME_PATTERN_ERROR = "err.user.pattern.first.name";
+    public static final String LAST_NAME_PATTERN_ERROR = "err.user.pattern.last.name";
+    public static final String USERNAME_PATTERN_ERROR = "err.user.pattern.username";
+    public static final String EMAIL_PATTERN_ERROR = "err.user.pattern.email";
+    public static final String USERNAME_EMAIL_PATTERN_ERROR = "err.user.pattern.username.email";
+    public static final String PASSWORD_PATTERN_ERROR = "err.user.pattern.password";
+}
--- src/main/java/com/jesusfc/springboot3java17/model/UserEntity.java
+++ src/main/java/com/jesusfc/springboot3java17/model/UserEntity.java
@@ -3,10 +3,15 @@ package com.jesusfc.springboot3java17.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+
+import static com.jesusfc.springboot3java17.model.UserConstant.*;
 
 @Getter
 @Setter
@@ -15,7 +20,10 @@ import lombok.ToString;
 @Entity(name = "user")
 public class UserEntity {
     @Id
-    @Column(name = "email", nullable = false)
+    @Column(name = "email", unique = true, nullable = false, length = EMAIL_MAX_CHAR)
+    @NotEmpty(message = EMAIL_NOT_EMPTY)
+    @Size(max = EMAIL_MAX_CHAR, message = EMAIL_CHAR_LENGTH)
+    @Email(regexp = USERNAME_EMAIL_PATTERN, message = EMAIL_PATTERN_ERROR)
     private String email;
     @Column(name = "name")
     private String name;

4027d53aa159f98aaf97d4bbac7fddb81b1d6840
Jesus <jesus.fdez.caraballo@gmail.com>
2023-02-20 15:33:30 +0100

    Update app to Open Api 2.0.2

--- src/main/java/com/jesusfc/springboot3java17/Springboot3Java17Application.java
+++ src/main/java/com/jesusfc/springboot3java17/Springboot3Java17Application.java
@@ -2,9 +2,7 @@ package com.jesusfc.springboot3java17;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
-//@EnableSwagger2WebMvc
 @SpringBootApplication(scanBasePackages = "com.jesusfc.springboot3java17")
 public class Springboot3Java17Application {
     public static void main(String[] args) {

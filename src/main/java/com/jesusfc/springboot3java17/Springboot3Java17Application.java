f72ffc88aae4cd8b7c8a657d80a5a79c1c1402f8
Jesus <jesus.fdez.caraballo@gmail.com>
2023-02-20 10:49:17 +0100

    Update app

--- src/main/java/com/jesusfc/springboot3java17/Springboot3Java17Application.java
+++ src/main/java/com/jesusfc/springboot3java17/Springboot3Java17Application.java
@@ -2,7 +2,9 @@ package com.jesusfc.springboot3java17;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
+//@EnableSwagger2WebMvc
 @SpringBootApplication(scanBasePackages = "com.jesusfc.springboot3java17")
 public class Springboot3Java17Application {
     public static void main(String[] args) {

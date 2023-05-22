6b0fecd6e7c10ae6835f2642761dddc0e7703eed
Jesus <jesus.fdez.caraballo@gmail.com>
2023-05-22 13:29:43 +0200

    Create JDBC connection

---
+++ src/main/java/com/jesusfc/springboot3java17/utils/dataBase/OpenConnectionDB.java
@@ -0,0 +1,35 @@
+package com.jesusfc.springboot3java17.utils.dataBase;
+
+import java.sql.*;
+
+/**
+ * @author jesusfc
+ * Created on may 2023
+ */
+abstract class OpenConnectionDB {
+
+    static final String DB_URL = "jdbc:mysql://localhost/TUTORIALSPOINT";
+    static final String USER = "guest";
+    static final String PASS = "guest123";
+    static final String QUERY = "SELECT id, first, last, age FROM Employees";
+
+    public Connection getConn() {
+
+        // Open a connection
+        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(QUERY);) {
+            // Extract data from result set
+            while (rs.next()) {
+                // Retrieve by column name
+                System.out.print("ID: " + rs.getInt("id"));
+                System.out.print(", Age: " + rs.getInt("age"));
+                System.out.print(", First: " + rs.getString("first"));
+                System.out.println(", Last: " + rs.getString("last"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- src/main/java/com/jesusfc/springboot3java17/utils/filesOperations/ReaderFilesFromFolder.java
+++ src/main/java/com/jesusfc/springboot3java17/utils/filesOperations/ReaderFilesFromFolder.java
@@ -12,6 +12,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.sql.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
@@ -24,14 +25,71 @@ public class ReaderFilesFromFolder {
 
     private static final String NEW_LINE_SEPARATOR = "\n";
     private static final String SEPARATOR_CSV = ";";
-    private static final String FOLDER_LOCATION = "/home/jesusfc/Descargas/vimeo_meta_data/metadata/";
+    private static final String FOLDER_LOCATION = "/home/jesusfc/Escritorio/metadata_22_06/";
+
+
+    static final String DB_URL = "jdbc:mysql://157.230.102.76:3306/eshitv_test?autoReconnect=true&useSSL=false";
+    static final String USER = "root";
+    static final String PASS = "64xUP5Tv";
+
 
     public static void main(String[] args) throws IOException {
-        loadSimpleProductList();
-        loadCollectionProductList();
+
+        createInsertProducts();
+        //loadSimpleProductList();
+        //loadCollectionProductList();
     }
 
-    public static void loadSimpleProductList() throws IOException {
+    private static void createInsertProducts() {
+        String vimeo_categoria = "ZUMBA® y Baile deportivo";
+        Map<String, List<Product>> productsByCollection = getProductsByCollectionListMap();
+        List<Product> productsByCurrentCollections = productsByCollection.get(vimeo_categoria);
+        // System.out.println(productsByCurrentCollections.get(0));
+        Product product = productsByCurrentCollections.get(0);
+
+        String QUERY = "select id, code, title from product where code = 'V1234'";
+        String INSERT_QUERY = "insert into image (data) values ('IMAGE_VALUE')";
+        String SQL_INSERT_QUERY = INSERT_QUERY.replace("IMAGE_VALUE", product.getThumbnail_b64());
+        System.out.println(SQL_INSERT_QUERY);
+
+        // Open a connection
+        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(QUERY);
+        ) {
+
+
+            // Extract data from result set
+            while (rs.next()) {
+                // Retrieve by column name
+                System.out.print("ID: " + rs.getInt("id"));
+                System.out.print(", CODE: " + rs.getString("code"));
+                System.out.print(", TITLE: " + rs.getString("title"));
+            }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+/*
+
+       String insertSql = "insert into product (code, title, subtitle, description, duration, completed_time, fk_video, fk_trailer,\n" +
+                "                                 premiere, premiere_end_date, published, publication_date, publication_end_date,\n" +
+                "                                 fk_image, fk_image_portrait, fk_thumbnail, fk_thumbnail_portrait, producer,\n" +
+                "                                 producer_type, subtitle_idx, fk_club, level, material, mental_level, technical_level,\n" +
+                "                                 physical_level)\n" +
+                "values ();");
+
+
+        System.out.println(
+
+ */
+    }
+
+
+    private static void loadSimpleProductList() throws IOException {
         StringBuilder productCSVList = getProductCSVList();
 
         ByteArrayInputStream byteArrayOutputStream = new ByteArrayInputStream(String.valueOf(productCSVList).getBytes(StandardCharsets.UTF_8));
@@ -47,7 +105,7 @@ public class ReaderFilesFromFolder {
         }
     }
 
-    public static void loadCollectionProductList() throws IOException {
+    private static void loadCollectionProductList() throws IOException {
         StringBuilder productCSVList = getProductByCollectionCSVList();
 
         ByteArrayInputStream byteArrayOutputStream = new ByteArrayInputStream(String.valueOf(productCSVList).getBytes(StandardCharsets.UTF_8));
@@ -63,28 +121,8 @@ public class ReaderFilesFromFolder {
         }
     }
 
-    public static StringBuilder getProductByCollectionCSVList() {
-        List<Product> productList = loadProductList();
-        Set<String> collect = productList.stream().flatMap(product -> product.getCollections().stream()).collect(Collectors.toSet());
-        Map<String, List<Product>> productsByCollection = new HashMap<>();
-        collect.forEach(title -> {
-            productList.forEach(product -> {
-                product.getCollections().forEach(collection -> {
-                    if (collection.equalsIgnoreCase(title)) {
-                        if (productsByCollection.containsKey(title)) {
-                            List<Product> productList1 = productsByCollection.get(title);
-                            productList1.add(product);
-                            productsByCollection.put(title, productList1);
-                        } else {
-                            List<Product> productListNew = new ArrayList<>();
-                            productListNew.add(product);
-                            productsByCollection.put(title, productListNew);
-                        }
-                    }
-                });
-            });
-        });
-
+    private static StringBuilder getProductByCollectionCSVList() {
+        Map<String, List<Product>> productsByCollection = getProductsByCollectionListMap();
 
         StringBuilder sb = new StringBuilder();
         getCSVCollectionHeader(sb);
@@ -122,7 +160,31 @@ public class ReaderFilesFromFolder {
         return sb;
     }
 
-    public static StringBuilder getProductCSVList() {
+    private static Map<String, List<Product>> getProductsByCollectionListMap() {
+        List<Product> productList = loadProductList();
+        Set<String> collect = productList.stream().flatMap(product -> product.getCollections().stream()).collect(Collectors.toSet());
+        Map<String, List<Product>> productsByCollection = new HashMap<>();
+        collect.forEach(title -> {
+            productList.forEach(product -> {
+                product.getCollections().forEach(collection -> {
+                    if (collection.equalsIgnoreCase(title)) {
+                        if (productsByCollection.containsKey(title)) {
+                            List<Product> productList1 = productsByCollection.get(title);
+                            productList1.add(product);
+                            productsByCollection.put(title, productList1);
+                        } else {
+                            List<Product> productListNew = new ArrayList<>();
+                            productListNew.add(product);
+                            productsByCollection.put(title, productListNew);
+                        }
+                    }
+                });
+            });
+        });
+        return productsByCollection;
+    }
+
+    private static StringBuilder getProductCSVList() {
         List<Product> productList = loadProductList();
         StringBuilder sb = new StringBuilder();
         getCSVHeader(sb);
